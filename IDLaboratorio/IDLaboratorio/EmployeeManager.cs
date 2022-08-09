091e6df54f646801f64f0ad7a9769453232bda9d
yenneralayon142 <yenner.alayon1@misena.edu.co>
2022-08-08 23:38:33 -0500

    Clase Employee

---
+++ IDLaboratorio/IDLaboratorio/EmployeeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace IDLaboratorio
+{
+    internal class EmployeeManager
+    {
+        public class EmployeeManager
+        {
+            private readonly List<Employee> _employees;
+
+            public EmployeeManager()
+            {
+                _employees = new List<Employee>();
+            }
+
+            public void AddEmployee(Employee employee)
+            {
+                _employees.Add(employee);
+            }
+
+        }
+
+    }
+    
+}
